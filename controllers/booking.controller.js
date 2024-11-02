3c125ed22a8fa3c98168a25e985f8cdb41b4ed59
ets-gopi <gopi.m@entitledarts.com>
2024-11-02 16:52:03 +0530

    changes in booking controller

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -1,8 +1,9 @@
 // working with transactions in mongodb.
 const { bookingModel, mongoose } = require("../models/booking.model");
-
+const { roomModel } = require("../models/room.model");
 const createBooking = async (req, res, next) => {
   console.log(req.body);
+  let availability = {};
   // start the session for
   const session = await mongoose.startSession();
   // console.log("Session ID:", session.id);
@@ -13,7 +14,32 @@ const createBooking = async (req, res, next) => {
     session.startTransaction();
     //console.log("Transaction State:", session.transaction.state);
     //check the conflict in booking details.
-    const bookingdocs = await bookingModel.find({});
+    const bookingdocs = await bookingModel.find(
+      {
+        $and: [
+          { propertyId: req.body.propertyId },
+          { checkIn: { $lt: new Date(req.body.checkOut) } },
+          { checkOut: { $gt: new Date(req.body.checkIn) } },
+          {
+            bookingStatus: { $in: ["Confirmed", "Pending", "CheckedIn"] },
+          },
+        ],
+      },
+      { roomInfo: 1 }
+    );
+    console.log(bookingdocs);
+
+    if (bookingdocs.length !== 0) {
+      console.log(bookingdocs);
+    } else {
+      for (const room of req.body.roomInfo) {
+        const roomdoc = await roomModel.findOne(
+          { _id: room.roomId },
+          { maxOccupancy: 1, quantityAvailable: 1 }
+        );
+        console.log(roomdoc);
+      }
+    }
 
     await session.commitTransaction();
     console.log("Transaction State:", session.transaction.state);
@@ -35,16 +61,19 @@ const createBooking = async (req, res, next) => {
 const testing = async (req, res, next) => {
   const checkin = "2024-11-10";
   const checkout = "2024-11-16";
-  const bookingdocs = await bookingModel.find({
-    $and: [
-      { propertyId: "650d53c1a7dfc56e1f0b8bc3" },
-      { checkIn: { $lt: new Date(checkout) } },
-      { checkOut: { $gt: new Date(checkin) } },
-      {
-        bookingStatus: { $in: ["Confirmed", "Pending", "CheckedIn"] },
-      },
-    ],
-  });
+  const bookingdocs = await bookingModel.find(
+    {
+      $and: [
+        { propertyId: "650d53c1a7dfc56e1f0b8bc3" },
+        { checkIn: { $lt: new Date(checkout) } },
+        { checkOut: { $gt: new Date(checkin) } },
+        {
+          bookingStatus: { $in: ["Confirmed", "Pending", "CheckedIn"] },
+        },
+      ],
+    },
+    { roomInfo: 1 }
+  );
 
   res.send(bookingdocs);
 };
--- middleware/booking.validation.middleware.js
+++ middleware/booking.validation.middleware.js
@@ -1,54 +1,88 @@
-const Validator=require("../utils/validator");
-const {bookingSchema}=require("../utils/schemaforValidation");
-const {HttpError,createError}=require("../utils/customError");
-const {userModel,mongoose} = require("../models/user.model");
+const Validator = require("../utils/validator");
+const { bookingSchema } = require("../utils/schemaforValidation");
+const { HttpError, createError } = require("../utils/customError");
+const { userModel, mongoose } = require("../models/user.model");
+const { propertyModel } = require("../models/property.model");
 
 // const path=require("path");
 // const fs=require("fs");
+const parseBoolean = (value) => {
+  if (typeof value === "string") {
+    if (value.toLowerCase().trim() === "true") {
+      return true;
+    } else if (value.toLowerCase().trim() === "false") {
+      return false;
+    }
+  }
+  return value;
+};
 
-const bookingValidation=async(req,res,next)=>{
-    //console.log(req.body);
-    const {id}=req.payload;
-    try {
-        if(Object.keys(req.body).length === 0){
-            throw createError("Invalid Request.",400);
-        }
-        const isUserExit=await userModel.findById(id);
-        if(!isUserExit){
-                throw createError("user does not exist.",404);
-            }
-        if(!mongoose.Types.ObjectId.isValid(req.params.propertyId)){
-            throw createError("Invalid Property ID.",400);
-        }
-        
-        const bookingObj=new Validator(bookingSchema);
-        const {data,errors}=bookingObj.validate({...req.body,userId:id,propertyId:req.params.propertyId});
-        //console.log(data,errors);
-        if(Object.keys(errors).length > 0){
-            throw createError("Invalid Request.",400,{error:errors});
-        }
-        req.body=data;
-        next();      
-        
-    } catch (error) {
-        console.error(error);
-        if(error instanceof HttpError){
-            res.json({
-                status:false,
-                bcc:error.statusCode,
-                message:error.message,
-                error:error.error
-            })
-        }else{
-            res.json({
-                status:false,
-                bcc:500,
-                message:"Internal server Issuse.",
-            }) 
-        }
-        
-    }  
-    
-   
-}
-module.exports=bookingValidation;
+const parseNumber = (value) => {
+  if (typeof value === "string") {
+    const parsed = Number(value.trim() ? value.trim() : undefined);
+    //console.log(value,parsed,isNaN(parsed));
+    return isNaN(parsed) ? value : parsed;
+  }
+  return value;
+};
+const bookingValidation = async (req, res, next) => {
+  //console.log(req.body);
+  const { id } = req.payload;
+  try {
+    if (Object.keys(req.body).length === 0) {
+      throw createError("Invalid Request.", 400);
+    }
+    const isUserExit = await userModel.findById(id);
+    if (!isUserExit) {
+      throw createError("user does not exist.", 404);
+    }
+    const propertydocs = await propertyModel.find(
+      {},
+      {
+        _id: 1,
+      }
+    );
+    let propertyIds = [];
+    for (const obj of propertydocs) {
+      propertyIds.push(obj._id.toString());
+    }
+    if (
+      !propertyIds?.includes(
+        mongoose.Types.ObjectId.isValid(req.params.propertyId) &&
+          req.params.propertyId
+      )
+    ) {
+      throw createError("Invalid Property ID.", 400);
+    }
+
+    const bookingObj = new Validator(bookingSchema);
+    const { data, errors } = bookingObj.validate({
+      ...req.body,
+      userId: id,
+      propertyId: req.params.propertyId,
+    });
+    //console.log(data,errors);
+    if (Object.keys(errors).length > 0) {
+      throw createError("Invalid Request.", 400, { error: errors });
+    }
+    req.body = data;
+    next();
+  } catch (error) {
+    console.error(error);
+    if (error instanceof HttpError) {
+      res.json({
+        status: false,
+        bcc: error.statusCode,
+        message: error.message,
+        error: error.error,
+      });
+    } else {
+      res.json({
+        status: false,
+        bcc: 500,
+        message: "Internal server Issuse.",
+      });
+    }
+  }
+};
+module.exports = bookingValidation;
