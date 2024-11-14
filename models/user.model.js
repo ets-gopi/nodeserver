b05c43724db95c4851a038c39c02f4c6a924f2a4
ets-gopi <gopi.m@entitledarts.com>
2024-11-14 18:34:34 +0530

    changes for user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,56 +1,57 @@
-const mongoose=require("mongoose");
-const {Schema} =mongoose;
-const argon2 =require("argon2");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const argon2 = require("argon2");
 // user Schema
 
-const userSchema=new Schema({
-    username:{
-        type:String
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
     },
-    email:{
-        type:String
+    email: {
+      type: String,
     },
-    password:{
-        type:String
+    password: {
+      type: String,
     },
-    isAdmin:{
-        type:Boolean,
-        default:false
+    isAdmin: {
+      type: Boolean,
+      default: false,
     },
-    bookings:{
-        type:[String]
+    bookings: {
+      type: [String],
     },
-    propertyId:{
-        type:[{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"Property",
-        }],
-    }
+    propertyId: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Property",
+        },
+      ],
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
 
-},{
-    timestamps:true,
-    versionKey:false
+userSchema.pre("save", async function (next) {
+  console.log(this);
+  this.password = await argon2.hash(this.password);
+  if (!this.isAdmin) {
+    this.propertyId = undefined;
+  }
+  next();
 });
 
-
-userSchema.pre("save",async function(next){
-    console.log(this);
-    this.password = await argon2.hash(this.password);
-    if (!this.isAdmin) {
-        this.propertyId = undefined;
-      }
-    next();
-    
-})
-
-userSchema.methods.verifyPassword=async function(password){
-    //console.log(this);
-    const isVerified=await argon2.verify(this.password,password);
-    return isVerified;
-    
-}
+userSchema.methods.verifyPassword = async function (password) {
+  //console.log(this);
+  const isVerified = await argon2.verify(this.password, password);
+  return isVerified;
+};
 // create an model
 
-const userModel=mongoose.model('User',userSchema);
+const userModel = mongoose.model("User", userSchema);
 
-module.exports={userModel,mongoose};
+module.exports = { userModel, mongoose };
